cd69c03f45aace1016c75439c9250b5998751e7c
asafworld <gabrielasaf10@gmail.com>
2023-02-09 02:54:54 -0300

    [FEAT] Cria rota para projeto específico

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 import ManagerProvider from './Context/Provider';
 import './App.css';
 import Projects from './Pages/Projects/Projects';
+import Project from './Components/Projects/Project';
 
 function App() {
   return (
     <ManagerProvider>
       <Routes>
         <Route path='/' element={ <Projects /> } />
+        <Route path='/project/:id' element={ <Project /> } />
       </Routes>
     </ManagerProvider>
   );
