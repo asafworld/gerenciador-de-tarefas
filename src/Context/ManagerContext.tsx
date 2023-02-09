c6389da759ba2e380acd7849b0832f17eb38e91f
asafworld <gabrielasaf10@gmail.com>
2023-02-09 02:19:42 -0300

    [FEAT] Cria o Contexto a ser acessado por toda aplicação.

---
+++ src/Context/ManagerContext.tsx
@@ -0,0 +1,6 @@
+import { createContext } from 'react';
+import IContext from '../Entities/Interfaces/IContext';
+
+const ManagerContext = createContext({} as IContext)
+
+export default ManagerContext;
