426ecd8f5511d1648a29dcc2afaa2c6d1da41da3
asafworld <gabrielasaf10@gmail.com>
2023-02-11 16:36:44 -0300

    [CSS] Insere estilização a página de projetos

--- src/Pages/Projects/Projects.tsx
+++ src/Pages/Projects/Projects.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import CreateProject from "../../Components/Projects/CreateProject";
 import DisplayProjects from "../../Components/Projects/DisplayProjects";
+import './Projects.css';
 
 function Projects() {
   return(
-    <article>
+    <article className="projects-article">
+      <div className="header-div">
+        <h1>GERENCIADOR DE TAREFAS</h1>
+      </div>
       <CreateProject />
       <DisplayProjects />
     </article>
