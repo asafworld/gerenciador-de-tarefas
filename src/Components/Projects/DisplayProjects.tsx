6c9fabeb11ded0a170879c22e388b3c9c56ab9e2
asafworld <gabrielasaf10@gmail.com>
2023-02-11 16:30:07 -0300

    [FEAT] Adiciona botão de exclusão de projetos

--- src/Components/Projects/DisplayProjects.tsx
+++ src/Components/Projects/DisplayProjects.tsx
@@ -1,22 +1,47 @@
 import React, { useContext } from "react";
 import ManagerContext from "../../Context/ManagerContext";
 import IProject from "../../Entities/Interfaces/IProject";
+import getPercentage from "../../utils/percentage";
+import './DisplayProjects.css';
 
 function DisplayProjects() {
-  const { projects } = useContext(ManagerContext);
+  const { 
+    projects,
+    setProject,
+    setPresentProject,
+  } = useContext(ManagerContext);
 
+  function mainProject(project: IProject) {
+    setPresentProject(project)
+  }
+
+  function deleteProject(event: React.SyntheticEvent, id: number) {
+    event.preventDefault()
+    if (projects) {
+      const newProjects = projects.filter((proj) => proj.projectId !== id)
+      setProject(newProjects)
+    }
+  }
+  
   return(
-    <article>
+    <article className="display-projects-article">
       {
-        !projects?.length ? (<h1>Nenhum projeto cadastrado!</h1>) : (
-          <section>
+        !projects?.length ? (<h1 className="no-project-saved">Nenhum projeto cadastrado!</h1>) : (
+          <section className="display-projects-section">
             {
               projects.map((proj: IProject) => (
-                <div>
+                <div key={ proj.projectId } className="display-projects-div" onClick={ () => mainProject(proj) }>
                   <a href={`/project/${proj.projectId}`}>
                     <h2>{ proj.projectLabel }</h2>
-                    <p>{ proj.percent }</p>
+                    { proj.concluded ? (<p>Concluído em {proj.finishedAt}</p>) : (<p>{ getPercentage(proj.tasks) }</p>)}
                   </a>
+                  <button
+                    type='button'
+                    className='form-control remove-project-button'
+                    onClick={ (e) => deleteProject(e, proj.projectId)}
+                  >
+                    Excluir
+                  </button>
                 </div>
               ))
             }
