579044c9a2a6f58b5137deaa2899a20ba7b3bc17
asafworld <gabrielasaf10@gmail.com>
2023-02-11 16:29:06 -0300

    [FEAT] Adiciona função para checagem de input, bem como estilização

--- src/Components/Projects/CreateProject.tsx
+++ src/Components/Projects/CreateProject.tsx
@@ -1,48 +1,68 @@
 import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import ManagerContext from "../../Context/ManagerContext";
 import IProject from "../../Entities/Interfaces/IProject";
+import './CreateProject.css';
 
 function CreateProject() {
   const [label, setLabel] = useState<string>('');
   const [description, setLDescription] = useState<string>('');
-  const { projects, setProject } = useContext(ManagerContext);
-  
+  const { projects, setProject, setPresentProject, errorMessage, setErrorMessage } = useContext(ManagerContext);
+  const navigate = useNavigate();
+
+  function inputsCheck() {
+    if (label === '' || description === '') {
+      setErrorMessage('Insira um nome e uma descrição para o projeto.');
+      return false;
+    }
+    setErrorMessage(undefined)
+    return true;
+  }
+
   function projectCreator(event: React.SyntheticEvent) {
     event.preventDefault()
+    if (!inputsCheck()) return false;
+    const index = projects?.length
     const newProject: IProject = {
       projectLabel: label,
       description,
-      projectId: projects?.length !== 0
-        && projects !== undefined ? projects.length - 1 : 0,
+      finishedAt: '',
+      tasks: null,
+      projectId: index ? index : 0,
       percent: 0,
       concluded: false
     }
     if (projects?.length !== 0 && projects !== undefined) {
       setProject([...projects, newProject])
+      setPresentProject(newProject)
     } else {
       setProject([newProject])
+      setPresentProject(newProject)
     }
+    navigate(`/project/${index}`)
   }
   
   return(
-    <article>
-      <form>
-        <label>
-          Nome do Projeto:
+    <article className="create-project-article">
+      <form className="create-project-section">
+        <div className="create-project-div">
           <input
-            placeholder="Indentifique o projeto"
+            placeholder="Nome do Projeto:"
+            className="create-project-input"
             value={ label }
             onChange={ ({ target }) => setLabel(target.value) }
           />
-        </label>
-        <label>
           <textarea
             value={ description }
+            className="create-project-textarea"
+            placeholder='Descreva o projeto:'
             onChange={ ({ target }) => setLDescription(target.value) }
           />
-        </label>
+        </div>
+        { errorMessage && (<p className="error-message">{errorMessage}</p>)}
         <button
           type="submit"
+          className="form-control create-project-button"
           onClick={ (e) => projectCreator(e) }
         >
           Criar projeto!
