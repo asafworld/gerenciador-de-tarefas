b73eb8cbb74f8eb10f40d435cf84ac09f6a6764d
asafworld <gabrielasaf10@gmail.com>
2023-02-11 16:27:24 -0300

    [FEAT] Cria rota para tarefas /task/:id

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import ManagerProvider from './Context/Provider';
 import './App.css';
 import Projects from './Pages/Projects/Projects';
-import Project from './Components/Projects/Project';
+import Project from './Pages/ProjectTasks/Project';
+import Subtask from './Pages/Task/SubTask';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={ <Projects /> } />
         <Route path='/project/:id' element={ <Project /> } />
+        <Route path='/task/:id' element={ <Subtask /> } />
       </Routes>
     </ManagerProvider>
   );
