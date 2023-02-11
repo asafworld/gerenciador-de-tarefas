f0e8c80baff2120164b0c6002990673845566c9b
asafworld <gabrielasaf10@gmail.com>
2023-02-11 16:32:59 -0300

    [STYLE] Adiciona propriedades de erro a interface do contexto do app

--- src/Entities/Interfaces/IContext.tsx
+++ src/Entities/Interfaces/IContext.tsx
@@ -1,12 +1,11 @@
 import { Dispatch, SetStateAction } from "react";
 import IProject from "./IProject";
-import ITask from "./ITask";
 
 export default interface IContext {
   projects: IProject[] | undefined;
   setProject: Dispatch<SetStateAction<IProject[] | undefined>>;
-  tasks: ITask[] | undefined;
-  setTask: Dispatch<SetStateAction<ITask[] | undefined>>
   presentProject: IProject | undefined;
   setPresentProject: Dispatch<SetStateAction<IProject | undefined>>;
+  errorMessage: string | undefined;
+  setErrorMessage: Dispatch<SetStateAction<string | undefined>>;
 }
